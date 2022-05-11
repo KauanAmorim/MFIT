3e155041dff3f9b95a48424c3b63bfa5b2078151
AugustoReis82 <79484649+AugustoReis82@users.noreply.github.com>
2022-05-11 20:55:24 -0300

    Teste 123

--- src/components/navbar/account/sign-up/Form/Password/index.tsx
+++ src/components/navbar/account/sign-up/Form/Password/index.tsx
@@ -13,3 +13,30 @@ export function SignUpPassword() {
         />
     )
 }
+
+
+
+/*
+function validar(){
+  var senha = formData.senha.value;
+  var rep_senha = formData.rep_senha.value;
+
+  if(senha == "" || senha.length <= 5){
+    alert('preencha o campo senha com no mínimo 6 caracteres');
+    formData.senha.focus();
+    return false;
+  }
+
+  if(rep_senha == "" || rep_senha.length <= 5){
+    alert('preencha o campo senha com no mínimo 6 caracteres');
+    formData.rep_senha.focus();
+    return false;
+  }
+  
+  if (senha != rep_senha) {
+    alert('As senhas devem ser iguais!');
+    form1.senha.focus();
+    return false;
+  }
+
+} */
